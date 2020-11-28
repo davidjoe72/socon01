06cd5bd0e8354d30312e8f355ee734772953848b
gyuhojang <gyuho72@gmail.com>
2020-11-28 21:45:57 +0900

    Updates

---
+++ precache-manifest.dc50acb614c3e791ee73ffdfb922beaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df895957b05d56e83f0f1c5b22d59652",
+    "url": "/socon01/index.html"
+  },
+  {
+    "revision": "092ec0e79e5676e8e9ec",
+    "url": "/socon01/static/css/main.452f48fe.chunk.css"
+  },
+  {
+    "revision": "349e42017e33ecc3210a",
+    "url": "/socon01/static/js/2.b17d2c77.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socon01/static/js/2.b17d2c77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092ec0e79e5676e8e9ec",
+    "url": "/socon01/static/js/main.e3d2daa9.chunk.js"
+  },
+  {
+    "revision": "a73e008e4c4f9bdc509a",
+    "url": "/socon01/static/js/runtime-main.634b62a3.js"
+  }
+]);
